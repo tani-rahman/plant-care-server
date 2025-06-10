38a71671e0f315915dcd3df01073ed3daf7603f5
tani-rahman <c231457@ugrad.iiuc.ac.bd>
2025-06-11 00:06:59 +0600

    setup the auth

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("plantDB");
     const plantCollection = db.collection("plants");
     const myPlantCollection = db.collection("myPlants");
@@ -132,7 +132,7 @@ async function run() {
         })
         
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB");
   } finally {
   
